267c0f96dc98b719baee2c4379c42ef6ae862078
Aditya Tangirala <Aditya.Tangirala@ggktech.local>
2019-02-14 20:02:34 +0530

    add factory pattern project

---
+++ AllPairShortestPath/EmployeePortal/Repository/UserRepo.cs
@@ -0,0 +1,33 @@
+﻿using Domain.Enums;
+using Domain.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Repository
+{
+    public class UserRepo
+    {
+        /// <summary>
+        /// Retrieve User details from the repository
+        /// </summary>
+        /// <param name="userRoleChoice"></param>
+        /// <returns></returns>
+        public List<UserModel> GetUserDetails(UserRoleChoice userRoleChoice)
+        {
+            List<UserModel> usersList=null;
+            switch ((int)userRoleChoice)
+            {
+                case 1 :
+                        usersList = (List<UserModel>)DataSource._userList.Where(m => m.IsStudent=="yes").ToList<UserModel>();
+                         break;
+                case 2 :
+                        usersList = (List<UserModel>)DataSource._userList.Where(m => m.IsStudent=="no").ToList<UserModel>();
+                    break;
+                case 3:
+                    usersList = DataSource._userList;
+                    break;
+            }
+            return usersList;
+        }
+    }
+}
